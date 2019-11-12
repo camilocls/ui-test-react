aa3cedcd3e02d54cfe85edc29cf39a547898d485
camilocls <camiloclsx@gmail.com>
2019-11-11 22:34:04 -0500

    Initial commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = (props) => {
+  return (
+    <div className="container">
+      HomePage
+    </div>
+  );
+}
+
+export default HomePage;
