0b04a1573e7fcad06967e22089c6a2145fa2725c
camilocls <camiloclsx@gmail.com>
2019-11-12 23:12:12 -0500

    Add dynamic data to Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState, useRef } from 'react'
 import './Hero.scss'
-import image from '../../assets/pope.jpg'
 import thumbUp from '../../assets/thumb-up.svg'
 import iconWiki from '../../assets/icon-wiki.png'
 
-
 const CountDown = (props) => {
+  const { daysLeft } = props
   return (
     <div className="hero__countdown">
       <div className="container">
@@ -13,7 +12,7 @@ const CountDown = (props) => {
           <div className="hero__countdown-label">CLOSING IN</div>
           <div className="hero__countdown-count">
             <span>
-              <strong>22</strong>&nbsp;days
+              <strong>{daysLeft}</strong>&nbsp;days
             </span>
           </div>
         </div>
@@ -25,6 +24,7 @@ const CountDown = (props) => {
 const Hero = (props) => {
   const [offsetContent, setOffsetContent] = useState({ left: 0, top: 0 })
   const contentEl = useRef(null)
+  const { hero } = props
 
   useEffect(() => {
     const handleOffsetContent = () => {
@@ -40,19 +40,19 @@ const Hero = (props) => {
   }, [])
 
   return (
-    <div style={{ backgroundImage: `url(${image})` }} className="hero">
+    <div style={{ backgroundImage: `url(assets/${hero.image})` }} className="hero">
       <div className="container">
         <div className="hero__wrapper-content">
           <div className="hero__content" ref={contentEl}>
             <div style={{ 
-              backgroundImage: `url(${image})`,
+              backgroundImage: `url(assets/${hero.image})`,
               left: `-${offsetContent.left}px`,
               top: `-${offsetContent.top}px` 
             }} className="hero__blurred" />
             <div className="hero__sub-headline">What’s your opinion on</div>
-            <div className="hero__headline">Pope Francis?</div>
+            <div className="hero__headline">{hero.name}?</div>
             <div className="hero__text">
-              He’s talking tough on clergy sexual abuse, but is he just another papal pervert protector? (thumbs down) or a true pedophile punishing pontiff? (thumbs up) 
+              {hero.description}
             </div>
             <a className="hero__link" href="#wikipedia"> 
               <i className="hero__link-icon">
@@ -72,7 +72,7 @@ const Hero = (props) => {
           </div>
         </div>
       </div>
-      <CountDown />
+      <CountDown daysLeft={hero.days_left} />
     </div>
   );
 }
