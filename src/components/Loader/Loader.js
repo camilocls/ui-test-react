51ad8c586240ca84b69b48ab440ab0dd3ca476c0
camilocls <camiloclsx@gmail.com>
2019-11-13 00:35:19 -0500

    Add loader component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Loader.scss'
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="loader__bar">
+        <div className="loader__bar-animated" />
+      </div>
+    </div>
+  )
+}
+
+export default Loader
