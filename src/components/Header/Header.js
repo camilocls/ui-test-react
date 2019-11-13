f09503f7dac528c3ed8d376c517daf53767ffd5a
camilocls <camiloclsx@gmail.com>
2019-11-12 23:11:28 -0500

    Add mobile menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import SearchBar from '../SearchBar/SearchBar'
+import iconMenu from '../../assets/icon-menu.svg'
+import iconClose from '../../assets/icon-close-white.svg'
 import './Header.scss';
 
 const Header = (props) => {
   const { floating } = props
+  const [isMenuMobileOpen, setIsMenuMobileOpen] = useState()
+
   return (
     <header className={`header${floating ? ' header--floating' : ''}`}>
       <div className="container header__container">
-        <div className="header__logo-text">Rule of Thumb.</div>
+        <Link to="/" className="header__link-logo">
+          <div className="header__logo-text">Rule of Thumb.</div>
+        </Link>
         <div className="header__right">
-          <div className="header__menu">
+          <nav className="header__menu">
             <Link to="/past-trials" className="header__menu-item">Past Trials</Link>
             <Link to="/how-it-works" className="header__menu-item">How It Works</Link>
             <Link to="/login" className="header__menu-item">Log In / Sign Up</Link>
-          </div>
+          </nav>
           <SearchBar className="header__search" />
+          <button onClick={() => setIsMenuMobileOpen(true)} className="header__icon-menu-mobile">
+            <img src={iconMenu} alt="Open menu mobile" />
+          </button>
         </div>
       </div>
+
+      {isMenuMobileOpen && (
+        <div className="header__nav-mobile">
+          <nav className="header__nav-mobile-menu">
+            <Link to="/past-trials" className="header__nav-mobile-item">Past Trials</Link>
+            <Link to="/how-it-works" className="header__nav-mobile-item">How It Works</Link>
+            <Link to="/login" className="header__nav-mobile-item">Log In / Sign Up</Link>
+          </nav>
+          <button onClick={() => setIsMenuMobileOpen(false)} className="header__close">
+            <img src={iconClose} alt="Close menu mobile" />
+          </button>
+        </div>
+      )}
     </header>
   );
 }
